a7eeccd71f4bd4dbd3cd699eed5aaad30d8146f3
coffee <makrchrim@gmail.com>
2023-05-18 22:11:50 +0300

    shape drawing

--- src/main/java/com/ccat/core/WindowManager.java
+++ src/main/java/com/ccat/core/WindowManager.java
@@ -4,19 +4,19 @@ import com.ccat.core.listener.KeyListener;
 import com.ccat.core.listener.MouseListener;
 import com.ccat.core.renderer.Shader;
 import com.ccat.core.util.ShapeUtil;
-import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryStack;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+import static org.lwjgl.opengl.GL30.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public final class WindowManager {
@@ -28,7 +28,8 @@ public final class WindowManager {
     private GLFWErrorCallback errorCallback;
 
     private Shader shader;
-    private int vao;
+    private final int floatSize = Float.BYTES;
+    private int challengeIndex = -1;
 
     private WindowManager() { }
 
@@ -78,17 +79,20 @@ public final class WindowManager {
     }
 
     private void loop() {
-        initChallenge1();
+        this.shader = new Shader();
+        shader.compile();
+        shader.bind();
 
         while(!glfwWindowShouldClose(window)) {
-//            glClear(GL_COLOR_BUFFER_BIT);
-//            glClearColor(0.3f, 0.4f, 0.5f, 1);
+            glClearColor(0.3f, 0.4f, 0.5f, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
 
-            drawChallenge1();
-
+            drawChallenges();
 
             glfwSwapBuffers(window);
             glfwPollEvents();
+
+            selectChallengeDisplay();
 
             if(KeyListener.isKeyPressed(GLFW_KEY_ESCAPE)) {
                 closeWindow();
@@ -96,13 +100,15 @@ public final class WindowManager {
         }
     }
 
+    /**
+     * Challenge 1
+     */
+    private int vao1;
+    private int vbo1;
     private void initChallenge1() {
-        this.shader = new Shader();
-        shader.compile();
-
         // Generate VAO, VBO
-        this.vao = glGenVertexArrays();
-        glBindVertexArray(vao);
+        this.vao1 = glGenVertexArrays();
+        glBindVertexArray(vao1);
 
         try(MemoryStack stack = MemoryStack.stackPush()) {
             float[] twoTrisArray = ShapeUtil.getTwoTrianglesVertexArray();
@@ -111,14 +117,13 @@ public final class WindowManager {
             vertexBuffer.flip();
 
 
-            int vbo = glGenBuffers();
-            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            this.vbo1 = glGenBuffers();
+            glBindBuffer(GL_ARRAY_BUFFER, vbo1);
             glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
         }
 
         int positionSize = 3;
         int colorSize = 4;
-        int floatSize = Float.BYTES;
         int vertexSizeBytes = (colorSize + positionSize) * floatSize;
 
 
@@ -145,30 +150,231 @@ public final class WindowManager {
         );
     }
     private void drawChallenge1() {
-        shader.bind();
-
-        glBindVertexArray(vao);
+        glBindVertexArray(vao1);
         glDrawArrays(GL_TRIANGLES, 0, 6);
+    }
+    private void disposeChallenge1() {
+        glDeleteVertexArrays(vao1);
+        glDeleteBuffers(vbo1);
 
-        //Unbind
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
         glBindVertexArray(0);
-
-        shader.unbind();
     }
 
-    private void testInputs() {
+    /**
+     * Challenge 2
+     */
+    private int vao2;
+    private int vbo2;
+    private int ebo2;
+    private void initChallenge2() {
+        float[] vertexArray = ShapeUtil.getSquareVertexArray();
+        this.vao2 = glGenVertexArrays();
+        glBindVertexArray(vao2);
 
-        if(KeyListener.isKeyPressed(GLFW_KEY_F1)) {
-            toggleFullScreen();
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            FloatBuffer vertices = stack.mallocFloat(vertexArray.length);
+            vertices.put(vertexArray);
+            vertices.flip();
+
+            this.vbo2 = glGenBuffers();
+            glBindBuffer(GL_ARRAY_BUFFER, vbo2);
+            glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+            this.ebo2 = glGenBuffers();
+            int[] elementArray = ShapeUtil.getSquareElementArray();
+            IntBuffer elementBuffer = stack.mallocInt(elementArray.length);
+            elementBuffer.put(elementArray);
+            elementBuffer.flip();
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo2);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+            int positionSize = 3;
+            int colorSize = 4;
+            int vertexSize = (colorSize + positionSize) * floatSize;
+
+            glVertexAttribPointer(0, colorSize, GL_FLOAT, false, vertexSize,0);
+            glEnableVertexAttribArray(0);
+
+            glVertexAttribPointer(1, positionSize, GL_FLOAT, false, vertexSize, colorSize * floatSize);
+            glEnableVertexAttribArray(1);
         }
-        if(MouseListener.getInstance().getXPos() > 50) {
-            System.out.println("Mouse position is greater than 50");
+    }
+    private void drawChallenge2() {
+        glBindVertexArray(vao2);
+        glDrawElements(
+                GL_TRIANGLES,
+                ShapeUtil.getSquareElementArray().length,
+                GL_UNSIGNED_INT,
+                0
+        );
+    }
+    private void disposeChallenge2() {
+        glDeleteVertexArrays(vao2);
+        glDeleteBuffers(vbo2);
+        glDeleteBuffers(ebo2);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+    }
+
+    /**
+     * Challenge3
+     */
+    private int vao3;
+    private int vbo3;
+    private int ebo3;
+    private void initChallenge3() {
+        float[] vertexArray = ShapeUtil.getStarVertexArray();
+        int[] elementArray = ShapeUtil.getStarElementArray();
+
+        this.vao3 = glGenVertexArrays();
+        glBindVertexArray(vao3);
+
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            FloatBuffer vertices = stack.mallocFloat(vertexArray.length);
+            vertices.put(vertexArray).flip();
+
+            this.vbo3 = glGenBuffers();
+            glBindBuffer(GL_ARRAY_BUFFER, vbo3);
+            glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+            IntBuffer elementBuffer = stack.mallocInt(elementArray.length);
+            elementBuffer.put(elementArray).flip();
+
+            this.ebo3 = glGenBuffers();
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo3);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+            int positionSize = 3;
+            int colorSize = 4;
+            int vertexSize = (positionSize + colorSize) * floatSize;
+
+            glVertexAttribPointer(0, colorSize, GL_FLOAT,false, vertexSize, 0);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(1, positionSize, GL_FLOAT,false,vertexSize, colorSize * floatSize);
+            glEnableVertexAttribArray(1);
+        }
+    }
+    private void drawChallenge3() {
+        glBindVertexArray(vao3);
+        glDrawElements(GL_TRIANGLES,
+                ShapeUtil.getStarVertexArray().length,
+                GL_UNSIGNED_INT,
+                0
+                );
+    }
+    private void disposeChallenge3() {
+        glDeleteVertexArrays(vao3);
+        glDeleteBuffers(vbo3);
+        glDeleteBuffers(ebo3);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+    }
+
+    /**
+     * Challenge 4
+     */
+    private int vao4;
+    private int vbo4;
+    private int ebo4;
+    private void initChallenge4() {
+        this.vao4 = glGenVertexArrays();
+        glBindVertexArray(vao4);
+
+        float[] vertexArray = ShapeUtil.getSquareOutlineArray();
+        int[] elementArray = ShapeUtil.getSquareOutlineElementArray();
+
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            FloatBuffer vertices = stack.mallocFloat(vertexArray.length);
+            vertices.put(vertexArray).flip();
+
+
+            this.vbo4 = glGenBuffers();
+            glBindBuffer(GL_ARRAY_BUFFER, vbo4);
+            glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+            IntBuffer elementBuffer = stack.mallocInt(elementArray.length);
+            elementBuffer.put(elementArray).flip();
+
+            this.ebo4 = glGenBuffers();
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo4);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+            int positionSize = 3;
+            int colorSize = 4;
+            int vertexSize = (positionSize + colorSize) * floatSize;
+
+            glVertexAttribPointer(0, colorSize, GL_FLOAT,false,colorSize,0);
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(1, positionSize, GL_FLOAT,false,vertexSize,colorSize * floatSize);
+            glEnableVertexAttribArray(1);
+        }
+    }
+    private void drawChallenge4() {
+        glBindVertexArray(vao4);
+        glDrawElements(GL_LINES,10,GL_UNSIGNED_INT,0);
+    }
+    private void disposeChallenge4() {
+        glDeleteVertexArrays(vao4);
+        glDeleteBuffers(vbo4);
+        glDeleteBuffers(ebo4);
+
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+    }
+
+    private void disposeCurrentChallenge() {
+        switch (challengeIndex) {
+            case 1 -> disposeChallenge1();
+            case 2 -> disposeChallenge2();
+            case 3 -> disposeChallenge3();
+            case 4 -> disposeChallenge4();
+        }
+    }
+
+    private void drawChallenges() {
+        switch (challengeIndex) {
+            case 1 -> drawChallenge1();
+            case 2 -> drawChallenge2();
+            case 3 -> drawChallenge3();
+            case 4 -> drawChallenge4();
+            default -> {}
+        }
+    }
+
+    private void selectChallengeDisplay() {
+        if(KeyListener.isKeyPressed(GLFW_KEY_1)) {
+            disposeCurrentChallenge();
+            challengeIndex = 1;
+            initChallenge1();
+        } else if(KeyListener.isKeyPressed(GLFW_KEY_2)) {
+            disposeCurrentChallenge();
+            challengeIndex = 2;
+            initChallenge2();
+        } else if(KeyListener.isKeyPressed(GLFW_KEY_3)) {
+            disposeCurrentChallenge();
+            challengeIndex = 3;
+            initChallenge3();
+        } else if(KeyListener.isKeyPressed(GLFW_KEY_4)) {
+            disposeCurrentChallenge();
+            challengeIndex = 4;
+            initChallenge4();
+        } else if(KeyListener.isKeyPressed(GLFW_KEY_5)) {
+            disposeCurrentChallenge();
+            challengeIndex = 5;
         }
     }
 
     private void dispose() {
+        shader.unbind();
+
         glfwDestroyWindow(window);
         glfwTerminate();
         errorCallback.free();
--- src/main/java/com/ccat/core/util/ShapeUtil.java
+++ src/main/java/com/ccat/core/util/ShapeUtil.java
@@ -1,6 +1,22 @@
 package com.ccat.core.util;
 
 public class ShapeUtil {
+    public static float[] getSquareOutlineArray() {
+        return new float[] {
+                //color                 //position
+                1f, 1f, 1f, 1f,         -0.5f,  0.5f, 0f,   //Top Left      | 0
+                1f, 1f, 1f, 1f,          0.5f,  0.5f, 0f,   //Top Right     | 1
+                1f, 1f, 1f, 1f,         -0.5f, -0.5f, 0f,   //Bottom Left   | 2
+                1f, 1f, 1f, 1f,          0.5f, -0.5f, 0f    //Bottom Right  | 3
+        };
+    }
+    public static int[] getSquareOutlineElementArray() {
+        return new int[] {
+                3, 1,   1, 0,
+                0, 2,   2, 3,
+                3, 0
+        };
+    }
     public static float[] getSquareVertexArray() {
         return new float[] {
                 //color                 //position
@@ -27,6 +43,30 @@ public class ShapeUtil {
                 1f, 0f, 0f, 1f,     -0.5f,  0.5f, 0f,
                 1f, 1f, 0f, 1f,      0.5f, -0.5f, 0f,
                 0f, 0f, 1f, 1f,      0.5f, 0.5f, 0f
+        };
+    }
+
+    public static float[] getStarVertexArray() {
+        return new float[] {
+                //color         //position
+                0f,0f,1f,1f,     0.2f,  0.1f, 0f, // 1 - Bottom Right   | 0
+                0f,1f,0f,1f,     0.0f,  0.5f, 0f, // 1 - Top            | 1
+                1f,0f,1f,1f,    -0.2f,  0.1f, 0f, // 1 - Bottom Left    | 2
+
+                1f,0f,0f,1f,    -0.5f, -0.5f, 0f, // 2 - Bottom Left    | 3
+                1f,1f,0f,1f,     0.5f,  0.1f, 0f, // 2 - Top Right      | 4
+                1f,0f,1f,1f,    -0.2f,  0.1f, 0f, // 2 - Top Left       | 5
+
+                0f,0f,1f,1f,     0.5f, -0.5f, 0f, // 3 - Bottom Right   | 6
+                1f,0f,0f,1f,     0.2f,  0.1f, 0f, // 3 - Top Right      | 7
+                1f,1f,0f,1f,    -0.5f,  0.1f, 0f  // 3 - Top Left       | 8
+        };
+    }
+    public static int[] getStarElementArray() {
+        return new int[] {
+                7, 1, 5,
+                3, 4, 5,
+                6, 7, 8
         };
     }
 }
